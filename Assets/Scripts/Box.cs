0c2a28fbbe36400fe0b96090778375328c77ccac
DESKTOP-DOH5618\Aaron <axf8278@rit.edu>
2019-10-11 14:38:42 -0400

    created Box.cs
    Box script contains list of ingredients (public for now as we are hard coding things) as well as a propert to change whether or not the box is open. If the box is open. then it will display a gui to the bottem right corner (hopefully) showing what ingrediants it contains

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// author: Aaron Feld
+/// This script functions to regiulate the box objects on the page including keeping track of the ingrediants inside, and haveing a method for "opening" the box
+/// should be attached to Box components
+/// </summary>
+
+public class Box : MonoBehaviour
+{
+    // list of ingredients in the box
+    public List<Ingrediant> ingrediants;
+    // name will be the 
+    public string name;
+
+    // each box object has a property saying whether the box is open or not - either a single box is open, in which cases all other boxes are set to closed, or 
+    bool isOpen = false;
+
+    // seter for whether the box is open or not
+    public bool IsOpen
+    {
+        set { isOpen = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnGUI()
+    {
+
+        if (inOpen) {
+            for (int i = 0; i < ingrediants.Count; i++) {
+                GUI.Box(new Rect(Screen.width - 50 - i * 10, Screen, height - 50 - i*10, Screen.width, Screen.Height), ingrediants[i]);
+            }
+        }
+    }
+}
