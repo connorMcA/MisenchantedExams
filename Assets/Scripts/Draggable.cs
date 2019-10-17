d3d672969333521dee02d50f3cb6a8bf48e1bae7
Julianna <Julianna@LAPTOP-OJC01Q1O>
2019-10-17 16:15:28 -0400

    added functionality to dragging

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    // sets object position to mouse point.
+    // starting position
+    Vector3 startingPos;
+
+    // reference to cauldron
+    public GameObject cauldron;
+
+    // Stores object's position prior to dragging.
+    private void OnMouseDown()
+    {
+        startingPos = gameObject.transform.position;
+    }
+
+    // Sets object position to mouse point.
     void OnMouseDrag()
     {
-        float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        float distance_to_screen = Camera.main.WorldToScreenPoint(cauldron.transform.position).z;
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+    }
+
+    // Resets position when let go.
+    private void OnMouseUp()
+    {
+        transform.position = startingPos;
     }
 }
